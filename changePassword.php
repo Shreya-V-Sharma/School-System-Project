fbc86b7c064a8bbe85efe7056dc273d1cd26f50d
Shreya-V-Sharma <shreyasharmacanada@gmail.com>
2023-01-06 12:34:13 -0400

    changed the titles for these pages to match their names

--- changePassword.php
+++ changePassword.php
@@ -51,7 +51,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Home</title>
+    <title>Profile</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
